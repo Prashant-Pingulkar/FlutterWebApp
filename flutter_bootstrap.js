8d57e5dfa119b43350aaf7a268175c98676aff83
Prashant Pingulkar <prashantpingulkar18@gmail.com>
2024-08-05 17:20:30 +0530

    updated build files

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "434758449"
+    serviceWorkerVersion: "897901387"
   }
 });
